65bc71c9c76e7cfb3618ac0e7330c97dcc95ad1d
Danilo Couto <couto.danilo95@gmail.com>
2022-11-17 17:40:02 -0300

    feat metatags for all important pages

---
+++ src/components/Head/head_gettinghere.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+function HeadGettingHere() {
+  return (
+    <Head>
+    <title>Como chegar em Pipa</title>
+    <meta
+     name="Saiba como vir de carro, onibus, van, táxi ou carona"
+     content="Temos todos os detalhes e contatos para voce nao se perder ao vir para Praia da Pipa."
+    />
+    <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default HeadGettingHere;
--- src/pages/localizacao/index.tsx
+++ src/pages/localizacao/index.tsx
@@ -1,11 +1,13 @@
 import styles from '../../../styles/Home.module.css'
 import Footer from '../../components/Footer/footer';
+import HeadLocation from '../../components/Head/head_location';
 import Header from '../../components/Header/header';
 import Localizacao from '../../components/Localizacao/localizacao';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <HeadLocation />
       <Header/>
       <Localizacao/>
       <Footer/>
