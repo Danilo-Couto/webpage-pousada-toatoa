061a4b6fbe8e2c0e8c70f591f0c7ca92d63d80d0
Danilo Couto <couto.danilo95@gmail.com>
2022-11-17 18:11:12 -0300

    feat como chegar: blablacar

---
+++ src/components/Buttons/blablacar.tsx
@@ -0,0 +1,12 @@
+import styles from '../../../styles/Home.module.css'
+import { otas } from '../../consts/otas';
+import { handleRedirect } from '../../helpers';
+
+export default function BlablacarButton() {
+  return (
+    <button className={styles.button_blablacar}
+      onClick={() => handleRedirect('https://www.blablacar.com.br/')}>
+      <h3>BLABLA CAR</h3>
+    </button>
+  );
+}
