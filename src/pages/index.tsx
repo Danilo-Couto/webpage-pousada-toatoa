5416359a2a0f41af9d4b453d1d5d8a7c07a000ff
Danilo Couto <couto.danilo95@gmail.com>
2023-02-16 16:27:30 -0300

    update instagram token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Index({feed}) {
 
 export async function getStaticProps() {
   const instaUrl = `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,username,timestamp&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACESS_TOKEN}`
-  let instaFeed;
+  let instaFeed: any;
   try {
     const data = await fetch(instaUrl);
     const dataJson = await data.json();
