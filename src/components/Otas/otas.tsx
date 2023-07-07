10a9a67968649c236d72e04cb4436112c203b460
Danilo Couto <couto.danilo95@gmail.com>
2023-07-07 11:46:15 -0300

    refactor: remove others otas and add hbook button

--- src/components/Otas/otas.tsx
+++ src/components/Otas/otas.tsx
@@ -1,8 +1,8 @@
 import styles from '../../../styles/Home.module.css'
 import useTranslation from '../../hooks/useTranslation';
 import BookNowButton from '../Buttons/bookNow';
-import OtasButton from '../Buttons/otas';
-function Otas() {
+import HBook from '../HBook/HBook';
+function Otas() {''
   const {t} = useTranslation();
 
   return (
@@ -14,8 +14,8 @@ function Otas() {
         <h3 className={styles.description}>
         {t('otas_descricao')}
         </h3>
+        <HBook/>
         <BookNowButton/>
-        <OtasButton/>
       </div>
     </div>
   );
